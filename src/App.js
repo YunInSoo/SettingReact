a0243a3585fa9954054900000a6f13a70397258f
yuninsoo <yunis@wecompany.kr>
2020-10-31 14:56:36 +0900

    made react-router and React.component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router, Route } from "react-router-dom";
+import React from "react";
+import ComponentTest from "./components/ComponentTest";
+import ComponentTestTwo from "./components/ComponentTestTwo";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    routeChange: false,
+    movies: [1, 2, 3],
+  };
+
+  getMovies = async () => {
+    const json = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    if (json) {
+      const {
+        data: {
+          data: { movies },
+        },
+      } = json;
+
+      this.setState(current => ({
+        movies: movies,
+        routeChange: !current.routeChange,
+      }));
+    }
+  };
+
+  async componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { routeChange, movies } = this.state;
+
+    return (
+      <Router>
+        {routeChange ? (
+          <div>
+            <ul>
+              {movies.map((e, index) => (
+                <li key={index}>{e.id}</li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <Route exact path="/" component={ComponentTest} />
+        )}
+      </Router>
+    );
+  }
 }
 
 export default App;
